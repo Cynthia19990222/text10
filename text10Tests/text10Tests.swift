ea6334a15787ac5f0b5d91c4f0f1eb67ff53cce8
Cynthia19990222 <zxcccc987@qq.com>
2018-10-26 20:56:37 +0800

    Initial Commit

---
+++ text10Tests/text10Tests.swift
@@ -0,0 +1,34 @@
+//
+//  text10Tests.swift
+//  text10Tests
+//
+//  Created by s20171103197 on 2018/10/26.
+//  Copyright © 2018 s20171103197. All rights reserved.
+//
+
+import XCTest
+@testable import text10
+
+class text10Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
